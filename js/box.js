07c5fdaa55b42eadf4fad81e35a5dfb5945cec52
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-10-29 19:26:37 +0100

    Update box.js

--- js/box.js
+++ js/box.js
@@ -1,1 +1,51 @@
+"strict mode"
 
+export class Box {
+  /*
+  Rectangle drawn by using webgl element buffers.
+  */
+  gl;
+  vao;
+  ebo;
+  x;
+  y;
+  width;
+  height;
+  constructor(canvas, x, y, width, height) {
+    /*
+    Define position and dimensions and create webgl components.
+    */
+    this.gl = canvas.getContext("webgl2");
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    
+    this.vao = this.gl.createVertexArray();
+    this.gl.bindVertexArray(this.vao);
+    this.gl.enableVertexAttribArray(0);
+    this.gl.enableVertexAttribArray(1);
+  
+    this.vbo = this.gl.createBuffer();
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vbo);
+    this.gl.bufferData(this.gl.ARRAY_BUFFER, 4 * 5 * 32 / 8, this.gl.STATIC_DRAW);
+  
+    this.ebo = this.gl.createBuffer();
+    this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.ebo);
+    this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, new Uint32Array([
+      0, 1, 2,
+      0, 2, 3
+    ]), this.gl.STATIC_DRAW);
+    
+    this.gl.vertexAttribPointer(0, 2, this.gl.FLOAT, false, 5 * 32 / 8, 0);
+    this.gl.vertexAttribPointer(1, 3, this.gl.FLOAT, false, 5 * 32 / 8, 2 * 32 / 8);
+  }
+  
+  draw() {
+    /*
+    Draw rectangle with webgl.
+    */
+    this.gl.bindVertexArray(this.vao)
+    this.gl.drawElements(this.gl.TRIANGLES, 6, this.gl.UNSIGNED_INT, 0);
+  }
+}
