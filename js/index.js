92280249ca4accf1a866cf991b60a2526ecd9883
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-10-31 17:25:57 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ async function main() {
   /*
   Main function. Declared as asynchronous to make better use of promises and read files.
   */
-  window.document.title = "(0.1.75) Simple project";
+  window.document.title = "(0.1.76) Simple project";
   
   const keyboard = new input.Keyboard();
   window.addEventListener("keydown", event => keyboard.keydown(event));
@@ -39,7 +39,7 @@ async function main() {
   let ball = new Box(canvas, -0.025, -0.04375, 0.05, 0.0875);
   //let sqr = new Box(canvas, 0.3, 0.3, 0.140625, 0.25);
   let targets = new Array(30);
-  for(let target = 0; target < targets.length; ++target) targets[target] = new Box(canvas, (target * 0.25) % 2.125 + 0.017578125 - 1.0, /*1.0 - (0.03125 + 0.125) * ((target / 9.0) - (target / 9.0) % 1 + 1)*/1.0 - 0.125 - target * 0.02, 0.25 - 0.03515625, 0.125);
+  for(let target = 0; target < targets.length; ++target) targets[target] = new Box(canvas, (target * 0.25) % 2.125 + 0.017578125 - 1.0, /*1.0 - (0.03125 + 0.125) * ((target / 9.0) - (target / 9.0) % 1 + 1)*/1.0 - 0.125 - ((target / 8.0) - (target / 8.0) * 0.02, 0.25 - 0.03515625, 0.125);
   //for(let target = 0; target < 8; ++target) targets[target] = new Box(canvas, target * 0.25 + 0.03515625 - 1.0, 1.0 - 0.0625 - 0.125, 0.25 - 0.0703125, 0.125);
   //for(let target = 8; target < 15; ++target) targets[target] = new Box(canvas, (target - 8) * 0.25 + 0.03515625 - 1.0, 0.65, 0.25 - 0.0703125, 0.125);
   //for(let target = 15; target < 23; ++target) targets[target] = new Box(canvas, (target - 15) * 0.25 + 0.03515625 - 1.0, 1.0 - (0.0625 - 0.125) * 3.0, 0.25 - 0.0703125, 0.125);
