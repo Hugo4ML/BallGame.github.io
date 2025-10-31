21c6f24d6f9ef2b4850518531598ef779660c203
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-10-31 11:30:19 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ async function main() {
   /*
   Main function. Declared as asynchronous to make better use of promises and read files.
   */
-  window.document.title = "(0.1.57) Simple project";
+  window.document.title = "(0.1.58) Simple project";
   
   const keyboard = new input.Keyboard();
   window.addEventListener("keydown", event => keyboard.keydown(event));
@@ -73,7 +73,7 @@ async function main() {
     
     gl.useProgram(await program);
     box.draw();
-    //ball.draw();
+    ball.draw();
     //for(let target = 0; target < targets.length; ++target) targets[target].draw();
   }, 1000 / 60);
 }
