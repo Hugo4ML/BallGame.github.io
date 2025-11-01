eb8fc75cd25091050cf999683266d3f9c23d86cb
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-11-01 10:55:07 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ async function main() {
   /*
   Main function. Declared as asynchronous to make better use of promises and read files.
   */
-  window.document.title = "(0.1.81) Simple project";
+  window.document.title = "(0.1.82) Simple project";
   
   const keyboard = new input.Keyboard();
   window.addEventListener("keydown", event => keyboard.keydown(event));
@@ -36,7 +36,7 @@ async function main() {
   let backgroundColor = (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches)? [0.0, 0.0, 0.0]: [1.0, 1.0, 1.0];
   
   let box = new Box(canvas, -0.1, -0.86875, 0.2, 0.04375);
-  let ball = new Box(canvas, -0.0140625, -0.025, 0.028125, 0.05);
+  let ball = new Box(canvas, Math.random() - 0.5140625, -0.025, 0.028125, 0.05);
   //let sqr = new Box(canvas, 0.3, 0.3, 0.140625, 0.25);
   let targets = new Array(30);
   for(let target = 0; target < targets.length; ++target) targets[target] = new Box(canvas, (target * 0.25) % 1.875 - 0.982421875, 1.0 - 0.15625 * ((target / 7.5) - (target / 7.5) % 1 + 1), 0.21484375, 0.125);
