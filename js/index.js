a27d406245b04a1590badf7149259fc70e37fd29
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-11-03 12:19:25 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ async function main() {
   /*
   Main function. Declared as asynchronous to make better use of promises and read files.
   */
-  window.document.title = "(0.2.07) Simple project";
+  window.document.title = "(0.2.08) Simple project";
   
   const keyboard = new input.Keyboard();
   window.addEventListener("keydown", event => keyboard.keydown(event));
@@ -32,7 +32,7 @@ async function main() {
   gl.attachShader(program, await fragmentShader);
   gl.linkProgram(await program);
 
-  let ballXSpeed = -0.0005625;
+  let ballXSpeed = 0.001;//-0.0005625;
   let ballYSpeed = 0.001;
   
   let box = new Box(canvas, -0.1, -0.86875, 0.028125 * 8, 0.05, (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches)? [1.0, 1.0, 1.0]: [0.0, 0.0, 0.0]);
