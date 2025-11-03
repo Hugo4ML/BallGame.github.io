668338fe8d745aed36450d8d646e0c0fe7fd601d
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-11-03 19:09:50 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ async function main() {
   /*
   Main function. Declared as asynchronous to make better use of promises and read files.
   */
-  window.document.title = "(0.2.38) Simple project";
+  window.document.title = "(0.2.39) Simple project";
   
   const keyboard = new input.Keyboard();
   window.addEventListener("keydown", event => keyboard.keydown(event));
@@ -119,7 +119,7 @@ async function main() {
         const leftWall = {
           time: (-1.0 - ball.x) / ballXSpeed,
           f: () => {
-            ball.y += ballYSpeed * (-1.0 - ball.width) / ballXSpeed;
+            ball.y += ballYSpeed * (-1.0 - ball.x) / ballXSpeed;
             ball.x = -1.0;
             ballXSpeed *= -1.0
           }
