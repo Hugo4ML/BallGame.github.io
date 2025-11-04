3921bb6ae8b0f62b2c0a8a6cb727090ae2ce78e9
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-11-04 18:01:44 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ async function main() {
   /*
   Main function. Declared as asynchronous to make better use of promises and read files.
   */
-  window.document.title = "(0.2.83) Simple project";
+  window.document.title = "(0.2.84) Simple project";
   
   const keyboard = new input.Keyboard();
   window.addEventListener("keydown", event => keyboard.keydown(event));
@@ -58,7 +58,7 @@ async function main() {
       gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
     };
     //Reassign background and box colors.
-    if(window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    /*if(window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
       box.color = [1.0, 1.0, 1.0];
       ball.color = [1.0, 1.0, 1.0];
       for(let target = 0; target < targets.length; ++target) targets[target].color = [1.0, 1.0, 1.0];
@@ -68,7 +68,7 @@ async function main() {
       ball.color = [0.0, 0.0, 0.0];
       for(let target = 0; target < targets.length; ++target) targets[target].color = [0.0, 0.0, 0.0];
       backgroundColor = [1.0, 1.0, 1.0];
-    }
+    }*/
     
     //Check input.
     if(keyboard.ArrowRight.down && !keyboard.ArrowLeft.down) {
@@ -87,7 +87,7 @@ async function main() {
         ball.x = 1.0 - ball.width;
         bulletTime -= (1.0 - ball.x - ball.width) / ballXSpeed;
         ballXSpeed *= -1.0;
-        ball.color = [time % 1.0, time % 1.0 + 0.5, time % 1.0];
+        ball.color = [1.0, time % 1.0 + 0.5, time % 1.0];
       /*} else if(ball.x + ballXSpeed * bulletTime < -1.0) {
         ball.x = -1.0;
         bulletTime -= (-1.0 - ball.x) / ballXSpeed;
