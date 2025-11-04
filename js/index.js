971038ed6c1892f4fc169295249ceade82b687c5
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-11-04 23:00:09 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ async function main() {
   /*
   Main function. Declared as asynchronous to make better use of promises and read files.
   */
-  window.document.title = "(0.3.01) Simple project";
+  window.document.title = "(0.3.02) Simple project";
   
   const keyboard = new input.Keyboard();
   window.addEventListener("keydown", event => keyboard.keydown(event));
@@ -47,7 +47,7 @@ async function main() {
     //Calculate delta time.
     let deltaTime = Date.now() - time;
     time = Date.now();
-    /*
+    
     if(deltaInnerWidth !== window.innerWidth || deltaInnerHeight !== window.innerHeight) {
       //Resize page.
       let minimum = window.innerWidth / 16 <= window.innerHeight / 9? window.innerWidth / 16: window.innerHeight / 9;
@@ -80,12 +80,12 @@ async function main() {
       box.x -= 0.001125 * deltaTime;
       if(box.x < -1.0) box.x = -1.0;
     }
-    */
+    
     //Move ball.
     let bulletTime = deltaTime;
     while(bulletTime > 0.0) {
       if((0.0 < (1.0 - ball.x - ball.width) / ballXSpeed) && (bulletTime >= (1.0 - ball.x - ball.width) / ballXSpeed)) {
-        ball.x = 0.5;//1.0 - ball.width - 0.0000001;
+        ball.x = 1.0 - ball.width;
         bulletTime -= (1.0 - ball.x - ball.width) / ballXSpeed;
         ballXSpeed *= -1.0;
       /*} else if(ball.x + ballXSpeed * bulletTime < -1.0) {
