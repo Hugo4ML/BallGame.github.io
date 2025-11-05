b2528c6288908a6ebc57279a7d033a8dede8279e
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-11-05 09:09:37 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ async function main() {
   /*
   Main function. Declared as asynchronous to make better use of promises and read files.
   */
-  window.document.title = "(0.3.25) Simple project";
+  window.document.title = "(0.3.26) Simple project";
   
   const keyboard = new input.Keyboard();
   window.addEventListener("keydown", event => keyboard.keydown(event));
@@ -84,10 +84,12 @@ async function main() {
     //Move ball.
     let bulletTime = deltaTime;
     let products = 0;
+    let distances = ["", ""];
     while(bulletTime > 0.0) {
       if((0.0 < (1.0 - ball.x - ball.width) / ballXSpeed) && (bulletTime >= (1.0 - ball.x - ball.width) / ballXSpeed)) {
+        distances[products] = (1.0 - ball.x - ball.width);
         products++;
-        window.document.title = ": " + products + ", " + (1.0 - ball.x - ball.width) / ballXSpeed;
+        window.document.title = products[0] + "; " + products[1];
         ball.x = 1.0 - ball.width;
         bulletTime -= (1.0 - ball.x - ball.width) / ballXSpeed;
         ballXSpeed *= -1.0;
