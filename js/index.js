836aa33ff05259994930b49f2059df2556000f11
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-11-05 10:05:29 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ async function main() {
   /*
   Main function. Declared as asynchronous to make better use of promises and read files.
   */
-  window.document.title = "(0.3.27) Simple project";
+  window.document.title = "(0.3.28) Simple project";
   
   const keyboard = new input.Keyboard();
   window.addEventListener("keydown", event => keyboard.keydown(event));
@@ -98,6 +98,10 @@ async function main() {
         bulletTime -= (-1.0 - ball.x) / ballXSpeed;
         ballXSpeed *= -1.0;*/
       } else {
+        if(products == 0) {
+          distances[0] = ": " + (1.0 - ball.x - ball.width)  / ballXSpeed;
+          ditances[1] = ": " + (1.0 - ball.x - ball.width)  / ballXSpeed;
+        }
         ball.x += ballXSpeed * bulletTime;
         bulletTime = 0.0;
       }
