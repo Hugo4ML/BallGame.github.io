c44d1119a7e6cc7136cf6fbb0ba6b424e04cb5dc
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-11-05 08:14:26 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ async function main() {
   /*
   Main function. Declared as asynchronous to make better use of promises and read files.
   */
-  window.document.title = "(0.3.05) Simple project";
+  window.document.title = "(0.3.06) Simple project";
   
   const keyboard = new input.Keyboard();
   window.addEventListener("keydown", event => keyboard.keydown(event));
@@ -32,7 +32,7 @@ async function main() {
   gl.attachShader(program, await fragmentShader);
   gl.linkProgram(await program);
 
-  let ballXSpeed = 0.0005625 / 4.0;
+  let ballXSpeed = 0.0005625;
   let ballYSpeed = 0.001 / 2.0;
   
   let box = new Box(canvas, -0.1, -0.86875, 0.028125 * 8, 0.05, (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches)? [1.0, 1.0, 1.0]: [0.0, 0.0, 0.0]);
@@ -85,9 +85,11 @@ async function main() {
     let bulletTime = deltaTime;
     while(bulletTime > 0.0) {
       if((0.0 < (1.0 - ball.x - ball.width) / ballXSpeed) && (bulletTime >= (1.0 - ball.x - ball.width) / ballXSpeed)) {
-        ball.x = 1.0 - ball.width + 0.0000001;
+        ball.x = 1.0 - ball.width;
         bulletTime -= (1.0 - ball.x - ball.width) / ballXSpeed;
         ballXSpeed *= -1.0;
+        ball.color = 0.0005625 / 32.0;
+        ball.color = [1.0, 0.5, 0.0];
       /*} else if(ball.x + ballXSpeed * bulletTime < -1.0) {
         ball.x = -1.0;
         bulletTime -= (-1.0 - ball.x) / ballXSpeed;
