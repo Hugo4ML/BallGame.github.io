79e5f815891668799711252975b07f369a15c0d3
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-11-07 09:09:36 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ async function main() {
   /*
   Main function. Declared as asynchronous to make better use of promises and read files.
   */
-  window.document.title = "(0.3.68) Simple project";
+  window.document.title = "(0.3.69) Simple project";
   
   const keyboard = new input.Keyboard();
   window.addEventListener("keydown", event => keyboard.keydown(event));
@@ -85,7 +85,23 @@ async function main() {
     }
     
     //Move ball.
-    {
+    let bulletTime = deltaTime;
+    while(bulletTime > 0.0) {
+      let timeStep = bulletTime
+      if((1.0 - (ball.y + ball.height)) / ballYSpeed > 0.0 && (1.0 - (ball.y + ball.height)) / ballYSpeed < timeStep) {
+        timeStep = (1.0 - (ball.y + ball.height)) / ballYSpeed;
+      } else if((-1.0 - ball.y) / ballYSpeed && (-1.0 - ball.y) / ballYSpeed < timeStep) {
+        timeStep = (-1.0 - ball.y) / ballYSpeed;
+      }
+      ball.y += ballYSpeed * timeStep;
+      if(ball.y + ball.height == 1.0) {
+        ballYSpeed *= -1.0;
+      } else if(ball.y == -1.0) {
+        ballYSpeed *= -1.0;
+      }
+      bulletTime -= timeStep;
+    }
+    /*{
       let bulletTime = deltaTime;
       while(bulletTime > 0.0) {
         let timeStep = deltaTime;
@@ -109,7 +125,7 @@ async function main() {
         }
         bulletTime -= timeStep;
       }
-    }
+    }*/
     
     gl.clearColor(backgroundColor[0], backgroundColor[1], backgroundColor[2], 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
