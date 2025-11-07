ca6005b1e198979553ccf949ed118316ffbded1c
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-11-07 12:47:48 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ async function main() {
   /*
   Main function. Declared as asynchronous to make better use of promises and read files.
   */
-  window.document.title = "(0.3.76) Simple project";
+  window.document.title = "(0.3.78) Simple project";
   
   const keyboard = new input.Keyboard();
   window.addEventListener("keydown", event => keyboard.keydown(event));
@@ -108,7 +108,7 @@ async function main() {
         window.document.title = "Total failure...";
         ++fails;
       } else {
-        window.document.title = "(0.3.76) F: " + fails;
+        window.document.title = "(0.3.78) F: " + fails + " " + (bulletTime - deltaTime);
       }
     }
     /*{
