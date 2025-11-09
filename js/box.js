b45307e949ecc90a3d3d78fc91bdfbde0710bd9d
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-11-09 12:39:25 +0100

    Update box.js

--- js/box.js
+++ js/box.js
@@ -4,73 +4,84 @@ export class Box {
   /*
   Rectangle drawn by using webgl element buffers.
   */
-  gl;
-  vao;
-  ebo;
   x;
   y;
   width;
   height;
   color;
-  constructor(canvas, x, y, width, height, color) {
+  gl;
+  vao;
+  vbo;
+  ebo;
+  constructor(x, y, width, height, canvas, color) {
     /*
     Define position and dimensions and create webgl components.
     */
-    this.gl = canvas.getContext("webgl2");
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
-    this.color = [0.0, 0.0, 0.0];
-    
-    const boundVao = this.gl.getParameter(this.gl.VERTEX_ARRAY_BINDING);
-    const boundVbo = this.gl.getParameter(this.gl.ARRAY_BUFFER_BINDING);
-    
-    this.vao = this.gl.createVertexArray();
-    this.gl.bindVertexArray(this.vao);
-    
-    this.vbo = this.gl.createBuffer();
-    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vbo);
-    this.gl.bufferData(this.gl.ARRAY_BUFFER, 4 * 5 * 32 / 8, this.gl.STATIC_DRAW);
-    
-    this.ebo = this.gl.createBuffer();
-    this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.ebo);
-    this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, new Uint32Array([
-      0, 1, 2,
-      0, 2, 3
-    ]), this.gl.STATIC_DRAW);
-    
-    this.gl.vertexAttribPointer(0, 2, this.gl.FLOAT, false, 5 * 32 / 8, 0);
-    this.gl.enableVertexAttribArray(0);
-    this.gl.vertexAttribPointer(1, 3, this.gl.FLOAT, false, 5 * 32 / 8, 2 * 32 / 8);
-    this.gl.enableVertexAttribArray(1);
-    
-    this.gl.bindVertexArray(boundVao);
-    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, boundVbo);
+    if(canvas !== undefined) {
+      this.gl = canvas.getContext("webgl2");
+      
+      const boundVao = this.gl.getParameter(this.gl.VERTEX_ARRAY_BINDING);
+      const boundVbo = this.gl.getParameter(this.gl.ARRAY_BUFFER_BINDING);
+      
+      this.vao = this.gl.createVertexArray();
+      this.gl.bindVertexArray(this.vao);
+      
+      this.vbo = this.gl.createBuffer();
+      this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vbo);
+      this.gl.bufferData(this.gl.ARRAY_BUFFER, 4 * 5 * 32 / 8, this.gl.STATIC_DRAW);
+      
+      this.ebo = this.gl.createBuffer();
+      this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.ebo);
+      this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, new Uint32Array([
+        0, 1, 2,
+        0, 2, 3
+      ]), this.gl.STATIC_DRAW);
+      
+      this.gl.vertexAttribPointer(0, 2, this.gl.FLOAT, false, 5 * 32 / 8, 0);
+      this.gl.enableVertexAttribArray(0);
+      this.gl.vertexAttribPointer(1, 3, this.gl.FLOAT, false, 5 * 32 / 8, 2 * 32 / 8);
+      this.gl.enableVertexAttribArray(1);
+      
+      this.gl.bindVertexArray(boundVao);
+      this.gl.bindBuffer(this.gl.ARRAY_BUFFER, boundVbo);
+      
+      if(color !== undefined) this.color = color;
+      else this.color = [0.0, 0.0, 0.0];
+    } else {
+      this.gl = undefined;
+      this.vao = undefined;
+      this.ebo = undefined;
+    }
   }
   
   draw() {
     /*
     Update vertex data and draw box with new dimensions.
     */
-    const boundVbo = this.gl.getParameter(this.gl.ARRAY_BUFFER_BINDING);
-    
-    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vbo);
-    this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array([
-      this.x,              this.y,               this.color[0], this.color[1], this.color[2],
-      this.x + this.width, this.y,               this.color[0], this.color[1], this.color[2],
-      this.x + this.width, this.y + this.height, this.color[0], this.color[1], this.color[2],
-      this.x,              this.y + this.height, this.color[0], this.color[1], this.color[2]
-    ]), this.gl.STATIC_DRAW);
-    
-    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, boundVbo);
-    
-    //Draw rectangle with webgl.
-    const boundVao = this.gl.getParameter(this.gl.VERTEX_ARRAY_BINDING);
-    
-    this.gl.bindVertexArray(this.vao);
-    this.gl.drawElements(this.gl.TRIANGLES, 6, this.gl.UNSIGNED_INT, 0);
-
-    this.gl.bindVertexArray(boundVao);
+    if(this.gl !== undefined) {
+      const boundVbo = this.gl.getParameter(this.gl.ARRAY_BUFFER_BINDING);
+      
+      this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vbo);
+      this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array([
+        this.x,              this.y,               this.color[0], this.color[1], this.color[2],
+        this.x + this.width, this.y,               this.color[0], this.color[1], this.color[2],
+        this.x + this.width, this.y + this.height, this.color[0], this.color[1], this.color[2],
+        this.x,              this.y + this.height, this.color[0], this.color[1], this.color[2]
+      ]), this.gl.STATIC_DRAW);
+      
+      this.gl.bindBuffer(this.gl.ARRAY_BUFFER, boundVbo);
+      
+      //Draw rectangle with webgl.
+      const boundVao = this.gl.getParameter(this.gl.VERTEX_ARRAY_BINDING);
+      
+      this.gl.bindVertexArray(this.vao);
+      this.gl.drawElements(this.gl.TRIANGLES, 6, this.gl.UNSIGNED_INT, 0);
+  
+      this.gl.bindVertexArray(boundVao);
+    }
   }
 }
