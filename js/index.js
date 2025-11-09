19d689e2e1a4b49f6c28e5d78ca7a9831a506087
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-11-09 12:41:13 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ async function main() {
   /*
   Main function. Declared as asynchronous to make better use of promises and read files.
   */
-  window.document.title = "(0.4.13) Simple project";
+  window.document.title = "(0.4.14) Simple project";
   
   const keyboard = new input.Keyboard();
   window.addEventListener("keydown", event => keyboard.keydown(event));
@@ -35,10 +35,10 @@ async function main() {
   let ballXSpeed = 0.0;//0.0005625 / 4.0;
   let ballYSpeed = 0.001 / 4.0;
   
-  let box = new Box(canvas, -0.1, -1.0, 0.028125 * 8.0, 0.05, (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches)? [1.0, 1.0, 1.0]: [0.0, 0.0, 0.0]);
-  let ball = new Box(canvas, Math.random() - 0.5140625, -0.025, 0.028125, 0.05, (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches)? [1.0, 1.0, 1.0]: [0.0, 0.0, 0.0]);
+  let box = new Box(-0.1, -1.0, 0.028125 * 8.0, 0.05, canvas, (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches)? [1.0, 1.0, 1.0]: [0.0, 0.0, 0.0]);
+  let ball = new Box(Math.random() - 0.5140625, -0.025, 0.028125, 0.05, canvas, (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches)? [1.0, 1.0, 1.0]: [0.0, 0.0, 0.0]);
   let targets = new Array(30);
-  for(let target = 0; target < targets.length; ++target) targets[target] = new Box(canvas, (target * 0.25) % 1.875 - 0.982421875, 1.03125 - 0.1875 * ((target / 7.5) - (target / 7.5) % 1 + 1), 0.21484375, 0.125, (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches)? [1.0, 1.0, 1.0]: [0.0, 0.0, 0.0]);
+  for(let target = 0; target < targets.length; ++target) targets[target] = new Box((target * 0.25) % 1.875 - 0.982421875, 1.03125 - 0.1875 * ((target / 7.5) - (target / 7.5) % 1 + 1), 0.21484375, 0.125, canvas, (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches)? [1.0, 1.0, 1.0]: [0.0, 0.0, 0.0]);
   let backgroundColor = (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches)? [0.0, 0.0, 0.0]: [1.0, 1.0, 1.0];
 
   let fails = 0;
@@ -88,7 +88,7 @@ async function main() {
     }
     
     //Move ball.
-    let bulletTime = deltaTime;
+    /*let bulletTime = deltaTime;
     while(bulletTime > 0.0) {
       timeStep = bulletTime;
       let hitBox = {
@@ -103,95 +103,6 @@ async function main() {
       bulletTime -= timeStep;
       if((1.0 - (ball.y + ball.height)) / ballYSpeed == timeStep) ballYSpeed *= -1.0;
       if((-1.0 - hitBox.y) / ballYSpeed == timeStep) ballYSpeed *= -1.0;
-    }
-    /*if(ballMotion) {
-      let bulletTime = deltaTime;
-      while(bulletTime > 0.0) {
-        let prevBall = {
-          x: ball.x,
-          y: ball.y,
-          width: ball.width,
-          height: ball.height
-        };
-        let timeStep = bulletTime;//(1.0 - (ball.y + ball.height)) / ballYSpeed;
-        if((1.0 - (ball.y + ball.height)) / ballYSpeed > 0.0 && (1.0 - (ball.y + ball.height)) / ballYSpeed < timeStep) {
-          timeStep = (1.0 - (ball.y + ball.height)) / ballYSpeed;
-        }
-        ball.y += timeStep * ballYSpeed;
-        if((1.0 - (prevBall.y + prevBall.height)) / ballYSpeed == timeStep) {
-          ballYSpeed *= -1.0;
-        }
-      }
-      //ball.y = 1.0 - ball.height;
-      /*let bulletTime = deltaTime;
-      while(bulletTime > 0.0) {
-        let life = false;
-        let death = false;
-        let timeStep = bulletTime
-        if(((1.0 - (ball.y + ball.height)) / ballYSpeed) > 0.0 && ((1.0 - (ball.y + ball.height)) / ballYSpeed) < timeStep) {
-          timeStep = (1.0 - (ball.y + ball.height)) / ballYSpeed;
-          life = true;
-          //life = (1.0 - (ball.y + ball.height)) / ballYSpeed;
-        }
-        if(((-1.0 - ball.y) / ballYSpeed) > 0.0 && ((-1.0 - ball.y) / ballYSpeed) < timeStep) {
-          timeStep = (-1.0 - ball.y) / ballYSpeed;
-          death = true;
-          //death = (-1.0 - ball.y) / ballYSpeed;
-        }
-        ball.y += timeStep * ballYSpeed;
-        bulletTime -= timeStep;
-        if((ball.y + ball.height == 1.0)) {
-          ballYSpeed *= -1.0;
-        }
-        if((ball.y == -1.0)) {
-          ballYSpeed *= -1.0;
-        }
-      }*/
-    }
-    /*if(ballMotion) {
-      let bulletTime = deltaTime;
-      while(bulletTime > 0.0) {
-        let timeStep = bulletTime
-        if(((1.0 - (ball.y + ball.height)) / ballYSpeed) > 0.0 && ((1.0 - (ball.y + ball.height)) / ballYSpeed) < timeStep) {
-          timeStep = (1.0 - (ball.y + ball.height)) / ballYSpeed;
-        }
-        if(((-1.0 - ball.y) / ballYSpeed) > 0.0 && ((-1.0 - ball.y) / ballYSpeed) < timeStep) {
-          timeStep = (-1.0 - ball.y) / ballYSpeed;
-        }
-        ball.y += timeStep * ballYSpeed;
-        bulletTime -= timeStep;
-        if((ball.y + ball.height) == 1.0) {
-          ballYSpeed *= -1.0;
-        }
-        if(ball.y == -1.0) {
-          ballYSpeed *= -1.0;
-        }
-      }
-    }*/
-    /*{
-      let bulletTime = deltaTime;
-      while(bulletTime > 0.0) {
-        let timeStep = deltaTime;
-        let timeFrames = [(1.0 - (ball.x + ball.width)) / ballXSpeed, (-1.0 - ball.x) / ballXSpeed, (1.0 - (ball.y + ball.height)) / ballYSpeed, (-1.0 - ball.y) / ballYSpeed];
-        let acts = [() => {
-          ballXSpeed *= -1.0;
-        }, () => {
-          ballXSpeed *= -1.0;
-        }, () => {
-          ballYSpeed *= -1.0;
-        }, () => {
-          ballYSpeed *= -1.0;
-        }];
-        for(let timeFrame of timeFrames) {
-          if(timeFrame > 0.0 && timeFrame < timeStep) timeStep = timeFrame;
-        }
-        ball.x += timeStep * ballXSpeed;
-        ball.y += timeStep * ballYSpeed;
-        for(let index = 0; index < timeFrames.length; ++index) {
-          if(timeFrames[index] == timeStep) acts[index]();
-        }
-        bulletTime -= timeStep;
-      }
     }*/
     
     gl.clearColor(backgroundColor[0], backgroundColor[1], backgroundColor[2], 1.0);
