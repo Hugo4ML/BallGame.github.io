8ba0982ba7fa06c4b9a0f7bd18ed1537103f0a9f
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-11-10 12:14:26 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 "use strict";
 
-import {Box} from "./box.js";
+import {Box} from "./shapes.js";
 import * as input from "./input.js";
 
 async function main() {
   /*
   Main function. Declared as asynchronous to make better use of promises and read files.
   */
-  window.document.title = "(0.4.32) Simple project";
+  window.document.title = "(0.4.33) Simple project";
   
   const keyboard = new input.Keyboard();
   window.addEventListener("keydown", event => keyboard.keydown(event));
