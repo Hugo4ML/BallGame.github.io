75c13a8d0770b2744d9e7ce2e1b0db205bae9dc8
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-11-10 12:10:03 +0100

    Create circle.js

---
+++ js/circle.js
@@ -0,0 +1,22 @@
+"use strict"
+
+export class Circle {
+  x;
+  y;
+  radius;
+  gl;
+  vao;
+  vbo;
+  ebo;
+  constructor(x, y, radius, canvas, color) {
+    /*
+    Creates a circle of a given radius.
+    */
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    if(canvas !== undefined) {
+      radius * (2.0 ** 0.5);
+    }
+  }
+}
