d987f1b0cb83fb61bd8cc31c97f5b8ade422aa72
Hugo4ML <hugofernandezgarcia@maristasleon.net>
2025-11-11 12:24:35 +0100

    Create Shape.js

---
+++ js/Shape.js
@@ -0,0 +1,63 @@
+"use strict"
+
+export class Shape {
+  /*
+  Shaped defined by a set of vertices to be drawn by using webgl element buffers.
+  */
+  x;
+  y;
+  color;
+  gl;
+  vao;
+  vbo;
+  ebo;
+  constructor(x, y, canvas, color) {
+    /*
+    Define position and create webgl components.
+    */
+    this.x = x, this.y = y;
+    if(canvas !== undefined) {
+      this.gl = canvas.getContext("webgl2");
+      
+      const boundVao = this.gl.getParameter(this.gl.VERTEX_ARRAY_BINDING);
+      const boundVbo = this.gl.getParameter(this.gl.ARRAY_BUFFER_BINDING);
+      
+      this.vao = this.gl.createVertexArray();
+      this.gl.bindVertexArray(this.vao);
+      
+      this.vbo = this.gl.createBuffer();
+      this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vbo);
+      
+      this.ebo = this.gl.createBuffer();
+      this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.ebo);
+      
+      this.gl.vertexAttribPointer(0, 2, this.gl.FLOAT, false, 5 * 32 / 8, 0);
+      this.gl.enableVertexAttribArray(0);
+      this.gl.vertexAttribPointer(1, 3, this.gl.FLOAT, false, 5 * 32 / 8, 2 * 32 / 8);
+      this.gl.enableVertexAttribArray(1);
+      
+      this.gl.bindVertexArray(boundVao);
+      this.gl.bindBuffer(this.gl.ARRAY_BUFFER, boundVbo);
+      
+      if(color !== undefined) this.color = color;
+      else this.color = [0.0, 0.0, 0.0];
+    } else {
+      this.gl = undefined, this.vao = undefined, this.ebo = undefined;
+    }
+  }
+  
+  draw() {
+    /*
+    Draw vertex array object.
+    */
+    if(this.gl !== undefined) {
+      //Draw rectangle with webgl.
+      const boundVao = this.gl.getParameter(this.gl.VERTEX_ARRAY_BINDING);
+      
+      this.gl.bindVertexArray(this.vao);
+      this.gl.drawElements(this.gl.TRIANGLES, 6, this.gl.UNSIGNED_INT, 0);
+  
+      this.gl.bindVertexArray(boundVao);
+    }
+  }
+}
